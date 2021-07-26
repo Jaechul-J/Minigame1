8fba98e62753d4506166b648fe547719821e9f61
Jaechul Jung <johnnynv6@gmail.com>
2021-07-26 21:58:42 +0900

    System accountable for difference number inputs

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number';
 
-console.log(document.querySelector('.message').textContent);
-*/
+const secretNumber = Math.trunc(Math.random() * 20) + 1; // A random secretNumber between 1 - 20
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = document.querySelector('.guess').value;
+  const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess);
 
+  // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number!!';
+    document.querySelector('.message').textContent = 'No Number typed!';
+  }
+  // When player wins
+  else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+  }
+  //When guess is higher
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  // When guess is lower
+  else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
